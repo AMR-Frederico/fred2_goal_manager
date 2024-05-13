18e167a589c22a624a151298110030f25d11a934
Leticia <leticiarp2000@hotmail.com>
2024-05-12 23:37:05 -0300

    style: add comments and use ghost goals tolerance  as a param

--- fred2_goal_manager/scripts/goal_reached.py
+++ fred2_goal_manager/scripts/goal_reached.py
@@ -30,29 +30,24 @@ debug_mode = '--debug' in sys.argv
 
 class goal_reached(Node): 
     
-    robot = Pose()
+    robot = Pose()                          # Variables to hold robot and goal positions
     goal = Pose()
 
-    robot_in_goal = Bool()
+    robot_in_goal = Bool()                  # Boolean to indicate if the robot is in goal
     robot_in_goal.data = False
 
-    robot_state = 100
+    robot_state = 100                       # Variable to hold robot state, starts in a random value 
 
-    last_goal = False
+    last_goal = False                       # Flag to indicate if the last goal has been reached
 
-    mission_completed = Bool()
+    mission_completed = Bool()              # Boolean to indicate if the mission is completed
     mission_completed.data = False
 
+    sinalization_msg = Bool()               # Message for LED signalization
 
-    sinalization_msg = Bool()
-
-    # niveis de precisão dos ghost goals
-    HIGH_ACCURACY = 0.15 
-    NORMAL_ACCURACY = 0.30
-    LOW_ACCURACY = 0.60
-
-    waypoint_goal = False
-    accuracy_level = NORMAL_ACCURACY
+    waypoint_goal = False                   # Flag to indicate if the goal is a waypoint
+    
+    ghost_goal_tolerance = 0.0                    # Accuracy level for reaching the goal
 
 
     # starts with randon value 
@@ -82,79 +77,111 @@ class goal_reached(Node):
                          start_parameter_services=start_parameter_services, 
                          parameter_overrides=parameter_overrides)
         
-        
-        # quality protocol -> the node must not lose any message 
-        qos_profile = QoSProfile(
-            reliability=QoSReliabilityPolicy.RELIABLE, 
-            durability= QoSDurabilityPolicy.VOLATILE,
-            history=QoSHistoryPolicy.KEEP_LAST, 
-            depth=10, 
-            liveliness=QoSLivelinessPolicy.AUTOMATIC
-            
-        )
-
-
-        self.create_subscription(PoseStamped, 
-                                 'goal/current', 
-                                 self.goalCurrent_callback, 
-                                 qos_profile)
-        
-
-        self.create_subscription(Int16,
-                                 '/machine_states/robot_state',
-                                 self.robot_state_callback, 
-                                 qos_profile)
-        
-        self.create_subscription(Bool, 
-                                 'robot/in_last_goal', 
-                                 self.lastGoal_callback, 
-                                 qos_profile)
-        
-                    
-        self.create_subscription(Odometry,
-                                '/odom', 
-                                self.odom_callback, 
-                                qos_profile)
-
-
-        self.goalReached_pub = self.create_publisher(Bool, 
-                                                     'goal/reached', 
-                                                     qos_profile)
-        
-
-        self.missionCompleted_pub = self.create_publisher(Bool, 
-                                                          'goal/mission_completed', 
-                                                          qos_profile)
-
-        self.led_on = self.create_publisher(Bool, 
-                                            'goal/sinalization', 
-                                            qos_profile)
-
-
+        self.quality_protocol()
+        self.setup_publishers()
+        self.setup_subscribers()
 
         self.load_params()
         self.get_params()
-
 
         self.add_on_set_parameters_callback(self.parameters_callback)
 
 
 
+        
+    def quality_protocol(self):
+
+        self.qos_profile = QoSProfile(
+            reliability=QoSReliabilityPolicy.RELIABLE,  # Set the reliability policy to RELIABLE, ensuring reliable message delivery
+            durability= QoSDurabilityPolicy.VOLATILE,   # Set the durability policy to VOLATILE, indicating messages are not stored persistently
+            history=QoSHistoryPolicy.KEEP_LAST,         # Set the history policy to KEEP_LAST, storing a limited number of past messages
+            depth=10,                                   # Set the depth of the history buffer to 10, specifying the number of stored past messages
+            liveliness=QoSLivelinessPolicy.AUTOMATIC    # Set the liveliness policy to AUTOMATIC, allowing automatic management of liveliness
+            
+        )
+
+
+
+    def setup_subscribers(self):
+        
+        # ------- Receives the current goal 
+        self.create_subscription(PoseStamped, 
+                                 'goal/current', 
+                                 self.goalCurrent_callback, 
+                                 self.qos_profile)
+        
+        # ------- Current robot state 
+        self.create_subscription(Int16,
+                                 '/machine_states/robot_state',
+                                 self.robot_state_callback, 
+                                 self.qos_profile)
+        
+        # ------- Flag to indicate if the robot is heading to the last goal 
+        self.create_subscription(Bool, 
+                                 'robot/in_last_goal', 
+                                 self.lastGoal_callback, 
+                                 self.qos_profile)
+        
+        # ------- Current robot pose according to odom        
+        self.create_subscription(Odometry,
+                                '/odom', 
+                                self.odom_callback, 
+                                self.qos_profile)
+
+
+
+    def setup_publishers(self):
+        
+        # ------ Flag to indicate if the robot has reached the goal 
+        self.goalReached_pub = self.create_publisher(Bool, 
+                                                     'goal/reached', 
+                                                     self.qos_profile)
+        
+        # ------ Indicates if the robot has reached the last goal 
+        self.missionCompleted_pub = self.create_publisher(Bool, 
+                                                          'goal/mission_completed', 
+                                                          self.qos_profile)
+        
+        # ------ Request for enable the led strip for goal sinzalization 
+        self.led_on = self.create_publisher(Bool, 
+                                            'goal/sinalization', 
+                                            self.qos_profile)
+
+
+
+    # updates the parameters when they are changed by the command line
     def parameters_callback(self, params):
         
         for param in params:
             self.get_logger().info(f"Parameter '{param.name}' changed to: {param.value}")
 
 
-        if param.name == 'robot_in_goal_tolerence':
-            self.ROBOT_IN_GOAL_TOLERANCE = param.value
-
         if param.name == 'debug': 
             self.DEBUG = param.value
 
-        if param.name =='unit_test': 
+
+        if param.name == 'unit_test': 
             self.UNIT_TEST = param.value
+
+
+        if param.name == 'waypoint_goal_tolerance': 
+            self.WAYPOINT_GOAL_TOLERANCE = param.value 
+
+        
+        if param.name == 'high_accuracy': 
+            self.HIGH_ACCURACY_TOLERANCE = param.value
     
+        
+        if param.name == 'normal_accuracy': 
+            self.NORMAL_ACCURACY_TOLERANCE = param.value
+
+
+        if param.name == 'low_accuracy': 
+            self.LOW_ACCURACY_TOLERANCE = param.value
+        
+
+        if param.name == 'frequency': 
+            self.FREQUENCY = param.value 
 
 
         return SetParametersResult(successful=True)
@@ -166,26 +193,57 @@ class goal_reached(Node):
         self.declare_parameters(
             namespace='',
             parameters=[
-                ('robot_in_goal_tolerance', None, ParameterDescriptor(
-                    description='Tolerance for determining if the robot has reached its goal, in meters',
-                    type=ParameterType.PARAMETER_DOUBLE)),
-                ('debug', None, ParameterDescriptor(
-                    description='Enable debug prints for troubleshooting',
-                    type=ParameterType.PARAMETER_BOOL)),
-                ('unit_test', None, ParameterDescriptor(
-                    description='Allow the node to run isolated for unit testing',
-                    type=ParameterType.PARAMETER_BOOL))
+
+                ('high_accuracy', None, 
+                    ParameterDescriptor(
+                        description='High level of accuracy for determining if the robot has reached its goal, in meters',
+                        type=ParameterType.PARAMETER_DOUBLE)),
+
+                ('normal_accuracy', None, 
+                    ParameterDescriptor(
+                        description='Normal level of accuracy for determining if the robot has reached its goal, in meters',
+                        type=ParameterType.PARAMETER_DOUBLE)),
+
+                ('low_accuracy', None, 
+                    ParameterDescriptor(
+                        description='Low level of accuracy for determining if the robot has reached its goal, in meters',
+                        type=ParameterType.PARAMETER_DOUBLE)),
+                        
+                ('waypoint_goal_tolerance', None, 
+                    ParameterDescriptor(
+                        description='Tolerance for determining if the robot has reached its waypoint goal, in meters',
+                        type=ParameterType.PARAMETER_DOUBLE)),
+
+                ('debug', None, 
+                    ParameterDescriptor(
+                        description='Enable debug prints for troubleshooting',
+                        type=ParameterType.PARAMETER_BOOL)),
+                
+                ('unit_test', None, 
+                    ParameterDescriptor(
+                        description='Allow the node to run isolated for unit testing',
+                        type=ParameterType.PARAMETER_BOOL)),
+                
+                ('frequency', None, 
+                    ParameterDescriptor(
+                        description='Node frequency', 
+                        type=ParameterType.PARAMETER_INTEGER)),
             ]
         )
 
 
+    # Get parameters value 
     def get_params(self):
         
-        self.ROBOT_IN_GOAL_TOLERANCE = self.get_parameter('robot_in_goal_tolerance').value
-
         self.DEBUG = self.get_parameter('debug').value
         self.UNIT_TEST = self.get_parameter('unit_test').value
+        self.FREQUENCY = self.get_parameter('frequency').value
 
+        self.HIGH_ACCURACY_TOLERANCE = self.get_parameter('high_accuracy').value
+        self.NORMAL_ACCURACY_TOLERANCE = self.get_parameter('normal_accuracy').value
+        self.LOW_ACCURACY_TOLERANCE = self.get_parameter('low_accuracy').value 
+
+        self.WAYPOINT_GOAL_TOLERANCE = self.get_parameter('waypoint_goal_tolerance').value
 
 
         if self.UNIT_TEST: 
@@ -234,7 +292,7 @@ class goal_reached(Node):
             self.get_logger().warn("Service call failed %r" % (e,))
 
 
-
+    # Get robot current position 
     def odom_callback(self, odom_msg): 
 
         self.robot.position.x = odom_msg.pose.pose.position.x 
@@ -245,116 +303,112 @@ class goal_reached(Node):
 
     def goalCurrent_callback(self, goal_msg): 
         
+        # Update the goal position based on the received message
         self.goal.position.x = goal_msg.pose.position.x 
         self.goal.position.y = goal_msg.pose.position.y
 
+
+        # Determine the accuracy level and whether the goal is a waypoint based on the orientation
         if goal_msg.pose.orientation.z == 1.0: 
 
-            self.accuracy_level = self.LOW_ACCURACY
+            self.ghost_goal_tolerance = self.LOW_ACCURACY_TOLERANCE
             self.waypoint_goal = False
 
         
         elif goal_msg.pose.orientation.z == 2.0: 
 
-            self.accuracy_level = self.NORMAL_ACCURACY
+            self.ghost_goal_tolerance = self.NORMAL_ACCURACY_TOLERANCE
             self.waypoint_goal = False
 
         elif goal_msg.pose.orientation.z == 3.0: 
 
-            self.accuracy_level = self.HIGH_ACCURACY
+            self.ghost_goal_tolerance = self.HIGH_ACCURACY_TOLERANCE
             self.waypoint_goal = False
 
         else: 
-
+            print('no else')
             self.waypoint_goal = True
 
 
+    # Get current robot state 
     def robot_state_callback(self, robot_state): 
         
         self.robot_state = robot_state.data
 
 
-
+    # Flag to indicate if the robot is heading to the last goal 
     def lastGoal_callback(self, status): 
 
         self.last_goal = status.data
 
 
-
+    # Main function responsible for checking if the robot has reached the goal or completed the mission.
     def main(self): 
         
+        # Calculate linear error between robot and goal positions
         dx = self.goal.position.x - self.robot.position.x 
         dy = self.goal.position.y - self.robot.position.y 
 
         linear_error = hypot(dx, dy)
 
-        if self.waypoint_goal: 
+        # Set default values
+        self.robot_in_goal.data = False
+        self.mission_completed.data = False
+        self.sinalization_msg.data = False
 
 
-            if (linear_error < self.ROBOT_IN_GOAL_TOLERANCE) and self.robot_state == self.ROBOT_AUTONOMOUS and not self.last_goal: 
+        # Check if the robot is in autonomous mode
+        if self.robot_state == self.ROBOT_AUTONOMOUS: 
+            
+            # Check if the goal is a waypoint goal
+            if self.waypoint_goal: 
                 
-                self.robot_in_goal.data = True
+                # Check if the robot has reached the waypoint goal
+                if (linear_error < self.WAYPOINT_GOAL_TOLERANCE) and not self.last_goal: 
+                    
+                    self.get_logger().info('WAYPOINT goal reachead !!!!!!!! ')
+                    
+                    self.robot_in_goal.data = True
 
-                self.get_logger().warn('Goal Reached!!!!')
+                    self.sinalization_msg.data = True
+                    self.led_on.publish(self.sinalization_msg)
+                
 
-                self.sinalization_msg.data = True
+                # Check if the robot has reached the final waypoint goal
+                elif (linear_error < self.WAYPOINT_GOAL_TOLERANCE) and self.last_goal: 
+                    
+                    self.mission_completed.data = True 
+
+                    self.get_logger().info('Mission Completed !!!!')
+                    
+
+            else:   # for ghost goal 
+                
+                # Disable the LED sinalization
+                self.sinalization_msg.data = False 
                 self.led_on.publish(self.sinalization_msg)
 
+                # Check if the goal is a ghost goal
+                if (linear_error < self.ghost_goal_tolerance): 
+                    
+                    self.robot_in_goal.data = True
 
-            elif (linear_error < self.ROBOT_IN_GOAL_TOLERANCE) and self.robot_state == self.ROBOT_AUTONOMOUS and self.last_goal: 
-
-                
-                
-                self.mission_completed.data = True
-
-                self.get_logger().info('Mission Completed !!!!')
-                
-
-            else: 
-
-                
-                self.robot_in_goal.data = False
-
-                self.mission_completed.data = False
-
-                self.sinalization_msg.data = False
-        
-        
-        else: 
-
-            self.sinalization_msg.data = False
-
-            if (linear_error < self.accuracy_level) and self.robot_state == self.ROBOT_AUTONOMOUS and not self.last_goal: 
-                
-                self.robot_in_goal.data = True
-
-                self.get_logger().info('Goal Reached!!!!')
+                    self.get_logger().info('GHOST goal reached !!!!!')
 
 
-
-            else: 
-                
-                self.robot_in_goal.data = False
-
-                self.mission_completed.data = False
-
-
-
-
+        # Publish status messages
         self.missionCompleted_pub.publish(self.mission_completed)
-
         self.goalReached_pub.publish(self.robot_in_goal)
-
         self.led_on.publish(self.sinalization_msg)
 
 
 
         if debug_mode or self.DEBUG: 
 
-            self.get_logger().info(f'Ghost goal: {not self.waypoint_goal} | Accurancy level: {self.accuracy_level}')
-            self.get_logger().info(f'Goal X: {self.goal.position.x} | Goal Y: {self.goal.position.y} ')
-            self.get_logger().info(f'Delta X: {dx} | Delta Y: {dy}')
-            self.get_logger().info(f'Linear error: {linear_error} | Tolerance: {self.ROBOT_IN_GOAL_TOLERANCE} | Robo in goal: {self.robot_in_goal.data}\n')
+            self.get_logger().info(f'Goal X: {self.goal.position.x} | Goal Y: {self.goal.position.y} | Robo in goal: {self.robot_in_goal.data}')
+            self.get_logger().info(f'Delta X: {dx} | Delta Y: {dy} | Linear error: {linear_error}')
+            self.get_logger().info(f'Ghost goal: {not self.waypoint_goal} | Tolerance: {self.ghost_goal_tolerance}')
+            self.get_logger().info(f'Waypoint goal: {self.waypoint_goal} | Tolerance: {self.WAYPOINT_GOAL_TOLERANCE}\n')
 
 
 
@@ -368,7 +422,7 @@ if __name__ == '__main__':
     thread = threading.Thread(target=rclpy.spin, args=(node,), daemon=True)
     thread.start()
 
-    rate = node.create_rate(7)
+    rate = node.create_rate(node.FREQUENCY)
 
     try: 
         while rclpy.ok(): 
