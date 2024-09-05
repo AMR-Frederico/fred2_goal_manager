a0c0f6ea03084879f0e2a4d2bf5a44c1b8bcfc1a
Leticia <leticiarp2000@hotmail.com>
2024-09-05 14:08:09 -0300

    chore: improve debug configuration

--- fred2_goal_manager/scripts/debug.py
+++ fred2_goal_manager/scripts/debug.py
@@ -4,15 +4,16 @@ from rclpy.node import Node
 
 def goal_provider(node: Node): 
             
-    node.get_logger().info(f'Current goal: {node.current_goal.pose.position.x}, {node.current_goal.pose.position.y}, {node.current_goal.pose.orientation.z}| Index: {node.current_index} | Number of goals: {len(node.goals)} ')
-    node.get_logger().info(f'Goal reached: {node.goal_reached} | Last status: {node.last_goal_reached} | Update goal: {node.goal_reached > node.last_goal_reached} \n')
-    node.get_logger().info(f'Goal reset: {node.reset}')
+    node.get_logger().info(
+        
+        f'Current goal -> X: {node.current_goal.pose.position.x}  Y: {node.current_goal.pose.position.y}, Theta: {node.current_goal.pose.orientation.z}| Index: {node.current_index} | Number of goals: {len(node.goals)} | Goal reached: {node.goal_reached} | Last status: {node.last_goal_reached} | Update goal: {node.goal_reached > node.last_goal_reached} | Goal reset: {node.reset}'
+    )
 
 
 
 def goal_reached(node: Node): 
 
-    node.get_logger().info(f'Goal X: {node.goal.position.x} | Goal Y: {node.goal.position.y} | Robo in goal: {node.robot_in_goal.data}')
-    node.get_logger().info(f'Delta X: {node.dx} | Delta Y: {node.dy} | Linear error: {node.linear_error}')
-    node.get_logger().info(f'Ghost goal: {not node.waypoint_goal} | Tolerance: {node.tolerance}')
-    node.get_logger().info(f'Waypoint goal: {node.waypoint_goal} | Tolerance: {node.WAYPOINT_GOAL_TOLERANCE}\n')
+    node.get_logger().info(
+        
+        f'Goal ->  X: {node.goal.position.x}  Y: {node.goal.position.y} | Robo in goal: {node.robot_in_goal.data} | Delta ->  X: {node.dx}  Y: {node.dy} | Linear error: {node.linear_error} | Ghost goal: {not node.waypoint_goal} Tolerance: {node.tolerance} | Waypoint goal: {node.waypoint_goal} Tolerance: {node.WAYPOINT_GOAL_TOLERANCE}'
+    )
